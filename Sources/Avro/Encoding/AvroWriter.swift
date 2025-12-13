8066181d166f6492d3f3123330451554943e6199
flexlixrup <felixruppert@icloud.com>
2025-12-13 12:35:29 +0100

    fix: revert the optimized impl due to compat issues

--- Sources/Avro/Encoding/AvroWriter.swift
+++ Sources/Avro/Encoding/AvroWriter.swift
@@ -79,26 +79,6 @@ final class AvroWriter {
 
 	@inline(__always)
 	private func writeVarUInt(_ value: UInt64) {
-		#if compiler(>=6.2)
-			// Swift 6.2+ compilers
-
-			#if os(iOS) || os(macOS) || os(tvOS) || os(watchOS) || os(visionOS)
-				if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *) {
-					writeVarUInt_inlineArray(value)
-				} else {
-					writeVarUInt_fallback(value)
-				}
-			#else
-				writeVarUInt_inlineArray(value)
-			#endif
-
-		#else
-			writeVarUInt_fallback(value)
-		#endif
-	}
-
-	@inline(__always)
-	private func writeVarUInt_fallback(_ value: UInt64) {
 		var v = value
 		while (v & ~0x7F) != 0 {
 			buffer.append(UInt8((v & 0x7F) | 0x80))
@@ -106,28 +86,5 @@ final class AvroWriter {
 		}
 		buffer.append(UInt8(v & 0x7F))
 	}
-
-	#if compiler(>=6.2)
-		@available(iOS 26.0, macOS 26.0, watchOS 26, tvOS 26, visionOS 26, *)
-		@inline(__always)
-		private func writeVarUInt_inlineArray(_ value: UInt64) {
-			var tmp: InlineArray<10, UInt8> = .init(repeating: 0)
-			var count = 0
-			var v = value
-
-			while (v & ~0x7F) != 0 {
-				tmp[count] = UInt8((v & 0x7F) | 0x80)
-				count += 1
-				v >>= 7
-			}
-			tmp[count] = UInt8(v & 0x7F)
-			count += 1
-
-			let s = tmp.span
-			for i in 0 ..< count {
-				buffer.append(s[i])
-			}
-		}
-	#endif
 
 }
